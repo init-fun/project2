1504524ffca241c055f30c7df35e51dbe603bc29
inti-fun <sde.kapilraj@gmail.com>
2021-06-06 07:55:39 +0530

    url cleaning

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -62,7 +62,7 @@ DATABASES["default"].update(db_from_env)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["kapilraj.herokuapp.com", "127.0.0.1"]
 

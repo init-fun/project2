227b23da9dfc7163f5e81dd0517b9582de308a03
inti-fun <sde.kapilraj@gmail.com>
2021-06-06 17:19:31 +0530

    finished most commented and popular

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -51,7 +51,7 @@ DATABASES["default"].update(db_from_env)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["kapilraj.herokuapp.com", "127.0.0.1"]
 
